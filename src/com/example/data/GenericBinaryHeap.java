8b6cfd2a52985b2ff8510dfb1c99e673d5cece5f
Kaveh Ghaboosi <ghaboosi@lab126.com>
2015-03-18 15:31:39 -0700

    Optimized logic for heapification.

--- src/com/example/data/GenericBinaryHeap.java
+++ src/com/example/data/GenericBinaryHeap.java
@@ -31,48 +31,81 @@ public class GenericBinaryHeap<T extends Comparable<? super T>> {
 	}
 
 	public static <T extends Comparable<? super T>> GenericBinaryHeap<T>
-	    genericBinaryHeapFactory(T[] arrayOfElements)
+	    genericBinaryHeapFactory(final T[] arrayOfElements, HeapType heapType)
 	    		throws NullPointerException {
 		if (arrayOfElements == null) throw new NullPointerException();
-		int numberOfLevels = (int) Math.floor(Math.log(arrayOfElements.length));
+
+		int numberOfLevels = (int) Math.floor(Math.log10(arrayOfElements.length) / Math.log10(2));
 		for (int i = numberOfLevels - 1; i >= 0; i--)
 			for (int j = (int) Math.pow(2, i) - 1;
 				j < (int) Math.pow(2, i + 1) - 1;
-				j++) heapifyFromIndex(j, arrayOfElements);
-		return null; // TODO
+				j++) heapifyFromIndex(j, arrayOfElements, heapType);
+		for (int i = 0; i < numberOfLevels; i++)
+			for (int j = (int) Math.pow(2, i) - 1;
+				j < (int) Math.pow(2, i + 1) - 1;
+				j++) {
+				
+			}
+		return null;
 	}
 
+	/**
+	 * Recursively heapify the sub-tree rooted at pseudo
+	 * node with data value equal to array's index "j".
+	 */
 	private static <T extends Comparable<? super T>> void
-	    heapifyFromIndex(int j, final T[] arrayOfElements) {
-		if (j >= arrayOfElements.length - 1 || 2*j > arrayOfElements.length - 1)
-			return;
-
+	    heapifyFromIndex(int j, final T[] arrayOfElements, HeapType heapType) {
 		T temp;
-		if (2*j <= arrayOfElements.length - 1 &&
-			2*j + 1 > arrayOfElements.length - 1) {
-			if (arrayOfElements[j].compareTo(arrayOfElements[2*j]) > 0) {
+
+		// Return if the sub-tree's root is the array's
+		// last element or falls off the array's boundary
+		if (j >= arrayOfElements.length - 1 ||
+			2*j > arrayOfElements.length - 1) return;
+
+		// Swap the sub-tree's root & its left child if necessary
+		// when the right child falls off the array's boundary.
+		if (2*j == arrayOfElements.length - 1) {
+			if  ((heapType == HeapType.MIN &&
+				arrayOfElements[j].compareTo(arrayOfElements[2 * j]) > 0) ||
+				(heapType == HeapType.MAX &&
+				arrayOfElements[j].compareTo(arrayOfElements[2 * j]) < 0)) {
 				temp = arrayOfElements[j];
-				arrayOfElements[j] = arrayOfElements[2*j];
-				arrayOfElements[2*j] = temp;
+				arrayOfElements[j] = arrayOfElements[2 * j];
+				arrayOfElements[2 * j] = temp;
 			}
 			return;
 		}
 
-		if (2*j <= arrayOfElements.length - 1 &&
-			2*j + 1 <= arrayOfElements.length - 1) {
-			if (arrayOfElements[2*j].compareTo(arrayOfElements[j]) <= 0 &&
-				arrayOfElements[2*j].compareTo(arrayOfElements[2*j + 1]) <= 0) {
+		// Heapify the topmost tree rooted at array's index "j" when the
+		// root, its left & right children all fall into the array boundary.
+		if (2*j <= arrayOfElements.length - 2) {
+			if ((heapType == HeapType.MIN &&
+				arrayOfElements[2*j].compareTo(arrayOfElements[j]) <= 0 &&
+				arrayOfElements[2*j].compareTo(arrayOfElements[2*j + 1]) <= 0) ||
+				(heapType == HeapType.MAX &&
+				arrayOfElements[2*j].compareTo(arrayOfElements[j]) >= 0 &&
+				arrayOfElements[2*j].compareTo(arrayOfElements[2*j + 1]) >= 0)) {
 				temp = arrayOfElements[j];
 				arrayOfElements[j] = arrayOfElements[2*j];
 				arrayOfElements[2*j] = temp;
-				heapifyFromIndex(2*j, arrayOfElements);
-			} else if (arrayOfElements[2*j + 1].compareTo(arrayOfElements[j]) <= 0 &&
-				arrayOfElements[2*j + 1].compareTo(arrayOfElements[2*j]) <= 0) {
+				// Continue with sub-tree rooted at left child
+				heapifyFromIndex(2*j, arrayOfElements, heapType);
+				return;
+			}
+
+			if ((heapType == HeapType.MIN &&
+				arrayOfElements[2*j + 1].compareTo(arrayOfElements[j]) <= 0 &&
+				arrayOfElements[2*j + 1].compareTo(arrayOfElements[2*j]) <= 0) ||
+				(heapType == HeapType.MAX &&
+				arrayOfElements[2*j + 1].compareTo(arrayOfElements[j]) >= 0 &&
+				arrayOfElements[2*j + 1].compareTo(arrayOfElements[2*j]) >= 0)) {
 				temp = arrayOfElements[j];
 				arrayOfElements[j] = arrayOfElements[2*j + 1];
 				arrayOfElements[2*j + 1] = temp;
-				heapifyFromIndex(2*j + 1, arrayOfElements);
+				// Continue with sub-tree rooted at right child
+				heapifyFromIndex(2*j + 1, arrayOfElements, heapType);
+				return;
 			}
-		}			
+		}
 	}
 }
