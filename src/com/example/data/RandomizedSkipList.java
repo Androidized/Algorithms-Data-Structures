1910625a7791585211c4b86ca8afe7ec70492972
Kaveh Ghaboosi <kaveh.ghaboosi@gmail.com>
2015-04-03 00:57:30 -0700

    Improved skip list logic for insertion.

--- src/com/example/data/RandomizedSkipList.java
+++ src/com/example/data/RandomizedSkipList.java
@@ -2,6 +2,7 @@ package com.example.data;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Random;
 
 public final class RandomizedSkipList<T extends Comparable<? super T>> {
 	Node header;
@@ -54,7 +55,52 @@ public final class RandomizedSkipList<T extends Comparable<? super T>> {
 	}
 
 	public void insert(T data) {
-		
+		Node nodeToInsert;
+		Node nodeToCompare;
+		Node currentNode = this.header;
+		int currentLevel = this.header.rightNeighbors.size() - 1;
+		if (currentLevel == -1) {
+			// When the skip list is empty,
+			// do not randomize the insertion.
+			nodeToInsert = new Node(data);
+			this.header.rightNeighbors.add(nodeToInsert);
+			nodeToInsert.rightNeighbors.add(sentinel);
+			return;
+		}
+		while (true) {
+			do {
+				nodeToCompare = currentNode.rightNeighbors.get(currentLevel--);
+			} while ((nodeToCompare.equals(sentinel) ||
+					 nodeToCompare.data.compareTo(data) > 0) &&
+					 currentLevel != -1);
+			if (currentLevel == -1) {
+				Node next = currentNode.rightNeighbors.get(0);
+				int i = 0;
+				Random rand = new Random();
+				while (rand.nextInt() > 0.5) i++;
+				nodeToInsert = new Node(data, i);
+				currentNode.rightNeighbors.set(0, nodeToInsert);
+				int newNodeCurrentLevel = 0;
+				while (newNodeCurrentLevel <= i) {
+					if (next.equals(this.sentinel)) {
+						while (newNodeCurrentLevel++ <= i)
+							nodeToInsert.rightNeighbors.add(this.sentinel);
+						return;
+					}
+					if (newNodeCurrentLevel <= next.level) {
+						while (newNodeCurrentLevel <= i &&
+						       newNodeCurrentLevel <= next.level) {
+							nodeToInsert.rightNeighbors.add(next);
+							newNodeCurrentLevel++;
+						}
+					}
+					next = next.rightNeighbors.get(0);
+				}
+				return;
+			}
+			if (nodeToCompare.data.compareTo(data) == 0) return;
+			currentNode = nodeToCompare;
+		}
 	}
 
 	public void delete(T dta) {
