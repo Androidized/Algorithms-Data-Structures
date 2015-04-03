c943a40a8b079a36b9546b9b13a6553853a3ff81
Kaveh Ghaboosi <kaveh.ghaboosi@gmail.com>
2015-04-02 22:34:11 -0700

    Corrected the entire data structure associated with SkipList.

--- src/com/example/data/RandomizedSkipList.java
+++ src/com/example/data/RandomizedSkipList.java
@@ -1,62 +1,63 @@
 package com.example.data;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.ArrayList;
 
 public final class RandomizedSkipList<T extends Comparable<? super T>> {
-	BorderNode header;
-	BorderNode sentinel;
+	Node header;
+	Node sentinel;
+
+	public RandomizedSkipList() {
+		this.header = new Node();
+		this.sentinel = new Node();
+	} 
 
 	class Node {
-		int level;
 		T data;
-		Node[] rightNeighbors;
-
-		Node() {
-			this.level = -1;
-			this.data = null;
-			this.rightNeighbors = null;
-		}
-
-		@SuppressWarnings("unchecked")
-		Node(int level, T data) {
-			this.level = level;
-			this.data = data;
-			this.rightNeighbors = (Node[]) new Object[level + 1];
-		}
-
-		public void setRightNeighbor(final Node rightNeiborToPoint, int level) {
-			this.rightNeighbors[level] = rightNeiborToPoint;
-		}
-	}
-
-	class BorderNode {
+		int level;
 		List<Node> rightNeighbors;
 
-		BorderNode() {
-			this.rightNeighbors = new ArrayList<Node>();
+		Node() {
+			this.data = null;
+			this.level = 0;
+			this.rightNeighbors = new ArrayList<Node>(1);
 		}
 
-		public void setRightNeighbor(final Node rightNeighborToPoint) {
-			this.rightNeighbors.add(rightNeighborToPoint);
+		Node(T data) {
+			this.data = data;
+			this.level = 0;
+			this.rightNeighbors = new ArrayList<Node>(1);
+		}
+
+		Node(T data, int level) {
+			this.data = data;
+			this.level = level;
+			this.rightNeighbors = new ArrayList<Node>(level);
 		}
 	}
 
-	RandomizedSkipList() {
-		this.header = new BorderNode();
-		this.sentinel = new BorderNode();
+	public boolean find(T data) {
+		Node nodeToCompare;
+		Node currentNode = this.header;
+		int currentLevel = this.header.rightNeighbors.size() - 1;
+		if (currentLevel == -1) return false;
+		while (true) {
+			do {
+				nodeToCompare = currentNode.rightNeighbors.get(currentLevel--);
+				if (nodeToCompare.equals(sentinel)) return false;
+			} while (nodeToCompare.data.compareTo(data) > 0 &&
+					 currentLevel != -1);
+			if (currentLevel == -1) return false;
+			if (nodeToCompare.data.compareTo(data) == 0) return true;
+			currentNode = nodeToCompare;
+		}
 	}
 
 	public void insert(T data) {
-		Node currentNode = null;
-		// Begin with header. The last element in header's rightNeighbors list
-		// represents the topmost pointer to an intermediate node somewhere in
-		// the middle of skip list.
-		int currentLevels = this.header.rightNeighbors.size();
-		Node nextNodeOnRight = this.header.rightNeighbors.get(currentLevels - 1);
-		if (data.compareTo(nextNodeOnRight.data) >= 0) {
-			currentNode = nextNodeOnRight; 
-		}
+		
+	}
+
+	public void delete(T dta) {
 		
 	}
 }
