3025b4ac0e5e5624771ab305c32fb2b91db8d7e1
Kaveh Ghaboosi <kaveh.ghaboosi@gmail.com>
2015-04-24 21:38:37 -0700

    Improved the logics for insertion of a node into a SkipList.

--- src/com/example/data/RandomizedSkipList.java
+++ src/com/example/data/RandomizedSkipList.java
@@ -1,6 +1,7 @@
 package com.example.data;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Random;
 import java.util.Set;
@@ -55,44 +56,79 @@ public final class RandomizedSkipList<T extends Comparable<? super T>> {
 		// list containing the same data value as the query.
 		return new NodeEntry<Boolean, Node>(false, currentNode);
 	}
-	
+
 	public Node insert(T data) {
+		int currentLevel;
+		Node currentNode;
+		Node nodeToInsert;
+		Node nodeToCompare;
+		Node lastNodeTraversedDown;
 		NodeEntry<Boolean, Node> entry = find(data);
+		// Return the node that has identical
+		// data value as the input argument.
 		if (entry.getKey()) return entry.getValue();
 		else {
-
-			// Create the node to be inserted into the list.
+			// Get a handle to the node that is going to be
+			// the immediate left neighbor of the yet to be
+			// inserted node. 
 			Node leftNeighbor = entry.getValue();
+			// Create the new node to be inserted into the list.
 			Random rand = new Random(2 * this.listLevel - 1);
-			Node nodeToInsert = new Node(data, rand.nextInt() + 1);
-
-			if (leftNeighbor.level > nodeToInsert.level) {
-				nodeToInsert.rightNeighbors.put(1, leftNeighbor.rightNeighbors.get(1));
-				nodeToInsert.rightNeighbors.put(nodeToInsert.level, leftNeighbor.rightNeighbors.get(1));
-				for (int leftNeighborLevelPointer : leftNeighbor.rightNeighbors.keySet()) {
+			nodeToInsert = new Node(data, rand.nextInt() + 1);
+			nodeToInsert.rightNeighbors
+				.put(1, leftNeighbor.rightNeighbors.get(1));
+			// Ensure the inserted node points to a right-hand
+			// neighbor, whose level is greater than or equal
+			// to that of the inserted node.
+			currentNode = nodeToInsert;
+			// This is to ensure whatever level smaller than
+			// that of the inserted node is encountered will
+			// be recorded.
+			int lowestLevelPointed = -1;
+			while (!currentNode.equals(sentinel)) {
+				if (currentNode.level >= nodeToInsert.level) {
+					nodeToInsert.rightNeighbors.put(nodeToInsert.level, currentNode);
+					break;
+				} else {
+					if (lowestLevelPointed == -1 ||
+						lowestLevelPointed < currentNode.level) {
+						lowestLevelPointed = currentNode.level;
+						nodeToInsert.rightNeighbors.put(currentNode.level, currentNode);
+					}
+				}
+				currentNode = currentNode.rightNeighbors.get(1);
+			}
+			// Ensure the immediately left-hand neighbor
+			// correctly points to the inserted node.
+			if (leftNeighbor.level >= nodeToInsert.level) {
+				for (int leftNeighborLevelPointer :
+					 leftNeighbor.rightNeighbors.keySet()) {
 					if (leftNeighborLevelPointer <= nodeToInsert.level) {
-						leftNeighbor.rightNeighbors.remove(leftNeighborLevelPointer);
+						leftNeighbor.rightNeighbors
+							.remove(leftNeighborLevelPointer);
 					}
 				}
 				leftNeighbor.rightNeighbors.put(1, nodeToInsert);
 				leftNeighbor.rightNeighbors.put(nodeToInsert.level, nodeToInsert);
 			} else {
-				nodeToInsert.rightNeighbors.put(1, leftNeighbor.rightNeighbors.get(1));
 				leftNeighbor.rightNeighbors.clear();
 				leftNeighbor.rightNeighbors.put(1, nodeToInsert);
 				leftNeighbor.rightNeighbors.put(leftNeighbor.level, nodeToInsert);
-			}
-
-			// Update left neighbor(s) of inserted node to point to it.
-			int levelToUpdateFrom = 1;
-			if (leftNeighbor.level <= nodeToInsert.level) {
-				levelToUpdateFrom = leftNeighbor.level;
-			} else {
-				levelToUpdateFrom = nodeToInsert.level;
-			}
-			for (int i = levelToUpdateFrom; i > 0; i--) {
-				nodeToInsert.rightNeighbors.put(i, leftNeighbor.rightNeighbors.get(i));
-				leftNeighbor.rightNeighbors.put(i, nodeToInsert);
+				// Update nodes before the left neighbor to have
+				// references to the inserted node if necessary.
+				currentNode = this.header;
+				currentLevel = this.listLevel;
+				while (currentLevel > 1) {
+					nodeToCompare = currentNode.rightNeighbors.get(currentLevel);
+					if (nodeToCompare != null) {
+						if (data.compareTo(nodeToCompare.data) > 0) {
+							currentNode = nodeToCompare;
+						} else if (data.compareTo(nodeToCompare.data) < 0) {
+							lastNodeTraversedDown = currentNode;
+							currentLevel--;
+						}
+					} else currentLevel--;
+				}
 			}
 
 			return nodeToInsert;
